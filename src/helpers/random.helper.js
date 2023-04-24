ee65b657f5646b5249e0b06b264c986954dd38fa
한재훈 <gnsehfud88@gmail.com>
2023-04-24 14:31:47 +0900

    난수 생성 관련하여 함수 수정
    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random

--- src/helpers/random.helper.js
+++ src/helpers/random.helper.js
@@ -1,5 +1,7 @@
 let randomRange = (min, max) => {
-    return parseInt(Math.random() * (max - min) + min);
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
 };
 
 let randomWeight = (weightList) => {
